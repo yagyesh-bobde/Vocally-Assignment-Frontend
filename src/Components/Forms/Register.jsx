a4f7cfa4f8a1e0bd3f0ae22bc207d3995496827c
Yagyesh Bobde <yagyeshsrbobde@gmail.com>
2023-01-01 14:42:28 +0530

    updated form validation

--- src/Components/Forms/Register.jsx
+++ src/Components/Forms/Register.jsx
@@ -12,7 +12,7 @@ const Register = () => {
     email: ''
   })
   const [formErrors, setformErrors] = useState({})
-
+  const [canSubmit, setcanSubmit] = useState(true)
   const changeFormData = (e) => {
     setformData({
       ...formData ,
@@ -25,7 +25,11 @@ const Register = () => {
     e.preventDefault()
     setformErrors({})
     validateForm()
-    registerUserAPI()  
+
+    if (canSubmit) {
+      registerUserAPI()  
+    } 
+    setcanSubmit(true)
 }
 
   const registerUserAPI = async () => {
@@ -58,16 +62,21 @@ const validateForm = () => {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (formData.name.length === 0) {
     errors.name = "Name is required"
+    setcanSubmit(false)
   } else if (formData.name.length < 3) {
     errors.name = "Name must be alteast 3 characters"
+    setcanSubmit(false)
   } else if (formData.name.length > 10) {
     errors.name = "Name can have a maximum of 10 characters"
+    setcanSubmit(false)
   }
 
   if (formData.email.length === 0) {
     errors.email = "Email is required"
+    setcanSubmit(false)
   } else if ( !regex.test(formData.email) ) {
     errors.email = "Enter a valid email"
+    setcanSubmit(false)
   }
 
  setformErrors(errors)
@@ -85,10 +94,10 @@ const validateForm = () => {
   return (
     <form  onSubmit={(e) => {submitForm(e)}} className='register_form' >
       <input onFocus={(e) => focusReset(e)} name={"name"} value={formData.name} onChange={(e) => { changeFormData(e) }} type="text" placeholder='Enter your Name' className="px-3 register_input rounded-pill"></input>
-      { formErrors.name && <span id={"name_error"} className='error_text'>
+      { ( formErrors.name) && <span id={"name_error"} className='error_text'>
         <i class="fa-solid fa-circle-xmark"></i>  {formErrors.name}</span>}
       <input onFocus={(e) => focusReset(e)} name={"email"} value={formData.email} onChange={(e) => { changeFormData(e) }} type="text"  placeholder='Enter Your Email' className="px-3 register_input rounded-pill"></input>
-      {formErrors.email && <span id={"email_error"} className='error_text'>
+      {( formErrors.email) && <span id={"email_error"} className='error_text'>
         <i class="fa-solid fa-circle-xmark "></i> {formErrors.email}</span>}
       <input type='submit' className="register_input rounded-pill text-center" value={"Submit"} ></input>
     </form>
